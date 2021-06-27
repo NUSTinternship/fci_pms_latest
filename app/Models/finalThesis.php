37004937971da75acfb893447c05596c37f85dcc
Vetara Kaurimuje <61870069+vetara@users.noreply.github.com>
2021-06-27 15:32:22 +0200

    Update finalThesis.php

--- app/Models/finalThesis.php
+++ app/Models/finalThesis.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Thesis extends Model
+class FinalThesis extends Model
 {
     use HasFactory;
 
