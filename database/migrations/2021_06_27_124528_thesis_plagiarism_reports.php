49cff772eca10109df9f5306aa3a8d817dae3e66
Vetara Kaurimuje <61870069+vetara@users.noreply.github.com>
2021-06-27 14:55:59 +0200

    Created Appropriate  Models & Migrations For Thesis Documents

---
+++ database/migrations/2021_06_27_124528_thesis_plagiarism_reports.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ThesisPlagiarismReports extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('thesis_plagiarism_reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id'); // Foreign Key From User Table
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
